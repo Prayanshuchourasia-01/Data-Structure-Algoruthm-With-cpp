2231506bd24dc2854761b396a34fb7b9bbeb6ec4
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-12-25 10:49:52 +0530

    Day 3 Q2 Modified

--- C++/Day 3/q2.cpp
+++ C++/Day 3/q2.cpp
@@ -8,7 +8,7 @@ int main(){
     int size= 5 ;
     int nums[size] = {22,65,-34,98,-234};
     int minn =INT_MAX; 
-    int maxx=0;
+    int maxx=0;     // in place of 0 we can use INT_MIN also which is -infintity
 
     for(int i = 0 ; i<size;i++){
         if(nums[i]<minn){
