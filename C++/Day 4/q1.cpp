230cd21099e5d78170f5d2353100794448f5f177
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-12-26 14:59:58 +0530

    Day 4 Q1 Complete

---
+++ C++/Day 4/q1.cpp
@@ -0,0 +1,29 @@
+// •	Linear search 
+
+#include<iostream>
+using namespace std;
+int linearSearch(int arr[] , int size,int searchEle){
+    for (int i =0 ; i < size ; i++){
+        if(arr[i] == searchEle){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int size = 11; 
+    int arr[size] = {1,5,3,4,3,2,7,64,36,75,2};
+
+    int searchEle;
+    cout<<"Enter the Element to Search : ";
+    cin>>searchEle;
+    int result = linearSearch(arr,size,searchEle) ; 
+    if (result==-1){
+        cout<<"Element Not Found ";
+    }
+    else{
+        cout<<"Element Found At Index : " <<result ;
+    }
+    return 0;
+}
